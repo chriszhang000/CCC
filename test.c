a88b5bbde3da64e6d94ac29c17aa1d5d4790310a
Chris Zhang <czhang@dapasoft.com>
2017-01-05 15:39:26 -0500

    test
    there is a good change...test test

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define MAX_ISBN_LENGTH 13
+
+int main(void)
+{
+    char *isbn = (char*) malloc(MAX_ISBN_LENGTH + 1 * sizeof(char));
+    printf("Enter text: ");
+    scanf("%s", isbn);
+
+    printf("Entered: %s\n", isbn);
+    printf("Length: %d\n", strlen(isbn));
+    printf("second letter: %c\n", isbn[1]);
+    return 0;//asdfaklsfjl;dfkg
+}
